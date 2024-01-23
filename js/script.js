ce6b94350a48424b7208c11fbf24324d3f07c7f5
Amarildo <amarildos@outloo.it>
2024-01-23 15:45:27 +0100

    added array to html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 console.log('js ok');
 console.log('Vue js ok', Vue);
 
+
 const {createApp} = Vue;
-const app = createApp ({
-
+const app = createApp({
+    data() {
+      return {
+        lists: [
+          { id: 1, done: false, text: 'Fare la spesa' },
+          { id: 2, done: true, text: 'Lavare i capelli' },
+          { id: 3, done: false, text: 'Comprare una marca da bollo' },
+          { id: 4, done: false, text: 'Aggiornare il PC' }
+        ]
+      };
+    }
 });
-
+  
 app.mount('#root');
